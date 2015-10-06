add88fc053728495030a68cafbcdee97cedc5775
simpleapples <zangzhiya@gmail.com>
2015-10-06 16:58:08 +0800

    fix

--- fansky/Controllers/SAPhotoTimeLineViewController.m
+++ fansky/Controllers/SAPhotoTimeLineViewController.m
@@ -84,7 +84,9 @@ static NSUInteger PHOTO_TIME_LINE_COUNT = 40;
         [SAMessageDisplayUtils showErrorWithMessage:error];
     };
     
-    [SAMessageDisplayUtils showProgressWithMessage:@"正在刷新"];
+    if (refresh) {
+        [SAMessageDisplayUtils showProgressWithMessage:@"正在刷新"];
+    }
     [[SAAPIService sharedSingleton] userPhotoTimeLineWithUserID:self.userID sinceID:nil maxID:maxID count:PHOTO_TIME_LINE_COUNT success:success failure:failure];
 }
 
