926fd6d40436eded65cb92e43cb1a61d77b63601
simpleapples <zangzhiya@gmail.com>
2015-10-27 18:19:38 +0800

    search controller

---
+++ fansky/Controllers/SASearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SASearchViewController.h
+//  fansky
+//
+//  Created by Zzy on 10/27/15.
+//  Copyright © 2015 Zzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SASearchViewController : UITableViewController
+
+@end
---
+++ fansky/Controllers/SASearchViewController.m
@@ -0,0 +1,112 @@
+//
+//  SASearchViewController.m
+//  fansky
+//
+//  Created by Zzy on 10/27/15.
+//  Copyright © 2015 Zzy. All rights reserved.
+//
+
+#import "SASearchViewController.h"
+
+@interface SASearchViewController () <UISearchResultsUpdating>
+
+@property (strong, nonatomic) UISearchController *searchController;
+
+@end
+
+@implementation SASearchViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self updateInterface];
+}
+
+- (void)updateInterface
+{
+    UISearchController *searchController = [[UISearchController alloc] initWithSearchResultsController:self];
+    searchController.searchResultsUpdater = self;
+    self.searchController = searchController;
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - UISearchResultsUpdating
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
+{
+    
+}
+
+#pragma mark - UITableViewDelegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
