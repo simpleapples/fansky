560ca9120964a4325e738419d3e440508c2e975d
simpleapples <zangzhiya@gmail.com>
2015-10-27 17:10:15 +0800

    修复tableviewcell点击状态下按钮背景色丢失的问题

--- fansky/Views/SAFriendCell.m
+++ fansky/Views/SAFriendCell.m
@@ -33,8 +33,9 @@
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
+    UIColor *followButtonBackgroundColor = self.followButton.backgroundColor;
     [super setSelected:selected animated:animated];
-
+    self.followButton.backgroundColor = followButtonBackgroundColor;
 }
 
 - (void)prepareForReuse
